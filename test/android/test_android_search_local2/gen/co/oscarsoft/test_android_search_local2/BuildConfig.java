706dcc19b669db8f2201a2755ab8ee2123b9e24d
Oscar So <oscarso@hotmail.com>
2015-05-10 16:31:50 -0700

    Deleted test_android_local_search2

--- test/android/test_android_search_local2/gen/co/oscarsoft/test_android_search_local2/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package co.oscarsoft.test_android_search_local2;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
