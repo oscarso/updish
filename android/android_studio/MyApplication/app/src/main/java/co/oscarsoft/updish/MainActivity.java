0c2e00c73da3b87d03a3da005177abf39c5af77d
Oscar So <oscarso@hotmail.com>
2016-03-08 20:43:55 -0800

    Fixed splash screen

--- android/android_studio/MyApplication/app/src/main/java/co/oscarsoft/updish/MainActivity.java
+++ android/android_studio/MyApplication/app/src/main/java/co/oscarsoft/updish/MainActivity.java
@@ -1,6 +1,7 @@
 package co.oscarsoft.updish;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,11 +12,14 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 3000; //3 seconds
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -25,7 +29,19 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                //Intent i = new Intent(MainActivity.this, PlaceListActivity.class);
+                //startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 
     @Override
